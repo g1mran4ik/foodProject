e4b2d9c0b8d578eeaa3ee92c69a83234bf5d3915
Ruslan Gimranov <g1mran@bk.ru>
2022-10-24 14:20:53 +0300

    Modal creating(show/close)

--- js/script.js
+++ js/script.js
@@ -101,4 +101,45 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     setClock('.timer', deadline);
+
+    // Modal
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalCloseBtn = document.querySelector('[data-close]');
+
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', () => {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            // использование toggle
+            // modal.classList.toggle('show');
+            document.body.style.overflow = 'hidden';
+        });      
+    });
+
+    function closeModal() {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        // использование toggle
+        // modal.classList.toggle('show');
+        document.body.style.overflow = '';
+    }
+
+    modalCloseBtn.addEventListener('click', closeModal);
+ 
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            // плохая практика - отсутствие аргумента на ввод и использование event.targer
+            // modal.addEventListener('click', () => {
+            //     if (event.target === modal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === "Escape" && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
 });
