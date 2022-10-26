460f6180cb0f966c8b570c9d707014cd1219c094
Ruslan Gimranov <g1mran@bk.ru>
2022-10-26 17:30:20 +0300

    Calculator added ver.1

--- js/script.js
+++ js/script.js
@@ -605,5 +605,100 @@ window.addEventListener('DOMContentLoaded', () => {
             dots.forEach(dot => dot.style.opacity = '.5');
             dots[slideIndex - 1].style.opacity = 1;         
         });
+
+  
+
+
     });
+
+    // Calculator
+
+    const result = document.querySelector('.calculating__result span');
+    let sex = 'female', 
+        height, 
+        weight, 
+        age, 
+        ratio = 1.375;
+
+    function calcTotal() { //Если пользователь что-то не ввел, будет отображаться нужный текст(в данном случае "____")
+        if (!sex || !height || !weight || !age || !ratio) {
+            result.textContent = '____';
+            return;
+        }
+
+        if (sex == 'female') { // условия расчета по формулам в зависимости от выбранного пола
+            result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+        } else {
+            result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+        }
+    }
+
+    calcTotal();
+
+    function getStaticInformation(parentSelector, activeClass) {
+        const elements = document.querySelectorAll(`${parentSelector} div`);
+
+        // Вместо делегирования событий используется forEach
+        elements.forEach(elem => {
+            elem.addEventListener('click', (e) => {
+                if (e.target.getAttribute('data-ratio')) {
+                    ratio = +e.target.getAttribute('data-ratio');
+                } else {
+                    sex = e.target.getAttribute('id');
+                }
+    
+                elements.forEach(elem => {
+                    elem.classList.remove(activeClass);
+                });
+    
+                e.target.classList.add(activeClass);
+    
+                calcTotal();
+            });
+        });
+
+        // Делегирование событий в данном случае не практично, т.к. при клике на пустые места будет неприятный баг
+        // document.querySelector(parentSelector).addEventListener('click', (e) => {
+        //     if (e.target.getAttribute('data-ratio')) {
+        //         ratio = +e.target.getAttribute('data-ratio');
+        //     } else {
+        //         sex = e.target.getAttribute('id');
+        //     }
+
+        //     elements.forEach(elem => {
+        //         elem.classList.remove(activeClass);
+        //     });
+
+        //     e.target.classList.add(activeClass);
+
+        //     calcTotal();
+        // });
+    }
+
+    getStaticInformation('#gender', 'calculating__choose-item_active');
+    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+
+    function getDynamicInformation(selector) {
+        const input = document.querySelector(selector);
+
+        input.addEventListener('input', () => {
+            switch(input.getAttribute('id')) {
+                case 'height':
+                    height = +input.value;
+                    break;
+                case 'weight':
+                    weight = +input.value;
+                    break;
+                case 'age':
+                    age = +input.value;
+                    break;
+            }
+
+            calcTotal();
+        });
+    }
+
+    getDynamicInformation('#height');
+    getDynamicInformation('#weight');
+    getDynamicInformation('#age');
 });
