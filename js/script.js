d18e4224dc0316ce26332e8c629413785bcc7c47
Ruslan Gimranov <g1mran@bk.ru>
2022-10-26 17:58:13 +0300

    Calculator edit with localStorage ver.2

--- js/script.js
+++ js/script.js
@@ -614,11 +614,47 @@ window.addEventListener('DOMContentLoaded', () => {
     // Calculator
 
     const result = document.querySelector('.calculating__result span');
-    let sex = 'female', 
-        height, 
-        weight, 
-        age, 
+    
+    // Сохранение стандартных значений, введенных пользователем с помощью localStorage
+    let sex, height, weight, age, ratio;
+
+    if (localStorage.getItem('sex')) {
+        sex = localStorage.getItem('sex');
+    } else {
+        sex = 'female';
+        localStorage.setItem('sex', 'female');
+    }
+
+    if (localStorage.getItem('ratio')) {
+        ratio = localStorage.getItem('ratio');
+    } else {
         ratio = 1.375;
+        localStorage.setItem('ratio', 1.375);
+    }
+
+    function initLocalSettings(selector, activeClass) {
+        const elements = document.querySelectorAll(selector);
+
+        elements.forEach(elem => {
+            elem.classList.remove(activeClass);
+            if (elem.getAttribute('id') === localStorage.getItem('sex')) {
+                elem.classList.add(activeClass);
+            }
+            if (elem.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+                elem.classList.add(activeClass);
+            }
+        });
+    }
+
+    initLocalSettings('#gender div', 'calculating__choose-item_active');
+    initLocalSettings('.calculating__choose_big div', 'calculating__choose-item_active');
+
+    // Добавление стандартных значений для калькулятора
+    // let sex = 'female', 
+    //     height, 
+    //     weight, 
+    //     age, 
+    //     ratio = 1.375;
 
     function calcTotal() { //Если пользователь что-то не ввел, будет отображаться нужный текст(в данном случае "____")
         if (!sex || !height || !weight || !age || !ratio) {
@@ -635,16 +671,21 @@ window.addEventListener('DOMContentLoaded', () => {
 
     calcTotal();
 
-    function getStaticInformation(parentSelector, activeClass) {
-        const elements = document.querySelectorAll(`${parentSelector} div`);
-
+    // function getStaticInformation(parentSelector, activeClass) {
+    //     const elements = document.querySelectorAll(`${parentSelector} div`);
+    
+    // Вариант написания функции без использования делегирования
+    function getStaticInformation(selector, activeClass) {
+        const elements = document.querySelectorAll(selector);
         // Вместо делегирования событий используется forEach
         elements.forEach(elem => {
             elem.addEventListener('click', (e) => {
                 if (e.target.getAttribute('data-ratio')) {
                     ratio = +e.target.getAttribute('data-ratio');
+                    localStorage.setItem('ratio', +e.target.getAttribute('data-ratio'));
                 } else {
                     sex = e.target.getAttribute('id');
+                    localStorage.setItem('sex', e.target.getAttribute('id'));
                 }
     
                 elements.forEach(elem => {
@@ -675,13 +716,21 @@ window.addEventListener('DOMContentLoaded', () => {
         // });
     }
 
-    getStaticInformation('#gender', 'calculating__choose-item_active');
-    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+    getStaticInformation('#gender div', 'calculating__choose-item_active');
+    getStaticInformation('.calculating__choose_big div', 'calculating__choose-item_active');
 
     function getDynamicInformation(selector) {
         const input = document.querySelector(selector);
 
         input.addEventListener('input', () => {
+
+            // Добавляем условие подсветки блока если пользователь вводит несоответствующее значение
+            if (input.value.match(/\D/g)) { // если пользователь вводит в графу НЕ число
+                input.style.border = '1px solid red'
+            } else {
+                input.style.border = 'none';
+            }
+
             switch(input.getAttribute('id')) {
                 case 'height':
                     height = +input.value;
