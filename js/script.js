f535824418a7538ab28e041a9be89f3dbaea343d
Ruslan Gimranov <g1mran@bk.ru>
2022-10-26 16:49:37 +0300

    Regular expression added

--- js/script.js
+++ js/script.js
@@ -518,11 +518,24 @@ window.addEventListener('DOMContentLoaded', () => {
         dots.push(dot);
     } 
 
+    // Добавляем функцию для замены неЦифр на пустые символы
+    function deleteNotDigits(str) {
+        return +str.replace(/\D/g, '');
+    }
+
     next.addEventListener('click', () => {
-        if (offset === +width.slice(0, width.length - 2) * (slides.length - 1)) {
+        // if (offset === +width.slice(0, width.length - 2) * (slides.length - 1)) {
+        // Использование регулярных выражений для исключения букв
+        // if (offset === +width.replace(/\D/g, '') * (slides.length - 1)) {
+        // Заменяем строку на добавленную выше функцию
+        if (offset === deleteNotDigits(width) * (slides.length - 1)) {    
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2);
+            // offset += +width.slice(0, width.length - 2);
+            // offset += +width.replace(/\D/g, '');
+            offset += deleteNotDigits(width);
+
+
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -545,9 +558,13 @@ window.addEventListener('DOMContentLoaded', () => {
 
     prev.addEventListener('click', () => {
         if (offset == 0) {
-         offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+        //  offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+        //  offset = +width.replace(/\D/g, '') * (slides.length - 1);
+         offset = deleteNotDigits(width) * (slides.length - 1);
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            // offset -= +width.slice(0, width.length - 2);
+            // offset -= +width.replace(/\D/g, '');
+            offset -= deleteNotDigits(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -573,7 +590,9 @@ window.addEventListener('DOMContentLoaded', () => {
             const slideTo = e.target.getAttribute('data-slide-to');
 
             slideIndex = slideTo;
-            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+            // offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+            // offset = +width.replace(/\D/g, '') * (slideTo - 1);
+            offset = deleteNotDigits(width) * (slideTo - 1);
 
             slidesField.style.transform = `translateX(-${offset}px)`;
 
