b9dcbc9036e5bd127e8d7cce8449dc353ec47aa1
g1mran4ik <114910256+g1mran4ik@users.noreply.github.com>
2022-10-28 15:21:19 +0300

    Update slider.js

--- js/modules/slider.js
+++ js/modules/slider.js
@@ -12,7 +12,7 @@ const slides = document.querySelectorAll('.offer__slide'),
         prev = document.querySelector('.offer__slider-prev'),
         next = document.querySelector('.offer__slider-next'),
         total = document.querySelector('#total'),
-        current = document.querySelector('#current');
+        current = document.querySelector('#current'),
         // добавление из второго варианта
         slidesWrapper = document.querySelector('.offer__slider-wrapper'),
         slidesField = document.querySelector('.offer__slider-inner'),
